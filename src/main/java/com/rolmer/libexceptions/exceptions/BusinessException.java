60ede5d41b531857a93790b1a3abc02743a39502
Rolmer <rolmertelis@gmail.com>
2026-02-18 02:04:15 -0300

    remove construtor

--- src/main/java/com/rolmer/libexceptions/exceptions/BusinessException.java
+++ src/main/java/com/rolmer/libexceptions/exceptions/BusinessException.java
@@ -12,8 +12,4 @@ import org.springframework.http.HttpStatus;
 public class BusinessException extends RuntimeException{
     private final HttpStatus httpStatus;
 
-    public BusinessException(String message, HttpStatus httpStatus){
-        super(message);
-        this.httpStatus = httpStatus;
-    }
 }
