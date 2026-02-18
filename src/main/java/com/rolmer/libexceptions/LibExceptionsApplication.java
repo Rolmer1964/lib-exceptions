3076d46954f517e588e44236f808cc1c053ce639
Rolmer <rolmertelis@gmail.com>
2026-02-17 23:19:43 -0300

    Primeiro commit

---
+++ src/main/java/com/rolmer/libexceptions/LibExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.rolmer.libexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rolmer/libexceptions/dto/ErrorDetails.java
@@ -0,0 +1,4 @@
+package com.rolmer.libexceptions.dto;
+
+public class ErrorDetails {
+}
